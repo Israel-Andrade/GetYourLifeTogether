56cd144a4584ea84a6b377b0c745eb04ea773881
Salvador Hernandez <Salvador Hernandez>
2016-05-12 23:48:12 -0700

    Can now update and create events dyamically
    Signed-off-by: Salvador Hernandez <Salvador Hernandez>

---
+++ app/src/main/java/calendar4/com/example/sal/calendar4/data/Channel.java
@@ -0,0 +1,7 @@
+package calendar4.com.example.sal.calendar4.data;
+
+/**
+ * Created by sal on 5/12/2016.
+ */
+public class Channel {
+}
---
+++ app/src/main/java/calendar4/com/example/sal/calendar4/service/YahooWeatherService.java
@@ -0,0 +1,7 @@
+package calendar4.com.example.sal.calendar4.service;
+
+/**
+ * Created by sal on 5/12/2016.
+ */
+public class YahooWeatherService {
+}
